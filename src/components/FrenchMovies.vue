a5df26bb9c48f36b43a122ef9c553b55a6f3122a
GJimmyX <gilbertg.jimmy@gmail.com>
2022-04-12 16:40:38 +0200

    Modifs

--- src/components/FrenchMovies.vue
+++ src/components/FrenchMovies.vue
@@ -29,7 +29,7 @@
         },
         created: function() {
             axios
-            .get('https://api.themoviedb.org/3/discover/movie?api_key=3ea8988340d4ed715d28b9978346c29e&language=fr&region=FR&sort_by=popularity.desc&certification_country=us&include_adult=false&include_video=false&page=1&vote_count.gte=1000&with_original_language=fr&with_watch_monetization_types=flatrate')
+            .get('https://api.themoviedb.org/3/discover/movie?api_key=3ea8988340d4ed715d28b9978346c29e&language=fr&region=FR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&vote_count.gte=1000&with_original_language=fr&with_watch_monetization_types=flatrate')
             .then(res => {
                 this.movies = res.data.results;
             })
